058474bd086902d2961ff3d2be9a6b06c63f2b2f
Arnav <aarnavsalvi2007@gmail.com>
2026-06-13 14:07:57 +0530

    operators and variable

---
+++ operators.java
@@ -0,0 +1,11 @@
+public class operators {
+    public static void main(String[] args) {
+        int C1 = 3;
+        int C2 = ++C1;
+        int C3 = C1++;
+        System.out.println(C2);
+        System.out.println(C3);
+
+    }
+}
+        
---
+++ variable.java
@@ -0,0 +1,17 @@
+public class variable {
+    public static void main(String[] args) {
+       String name = "Arnav";
+       int age = 18;
+       long A1 = age;
+       long A2 = 8567998;
+       int A3 = (int)A2;
+       char C1 = 'k';
+       int C2 = (int)C1;
+       float B1 = 10.73f;
+       int B2 = (int)B1;
+
+       System.out.println(B2);
+
+
+    }
+}
