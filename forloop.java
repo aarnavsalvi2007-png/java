d77f222e6b81b1abc198b0dde104bb14b3920ba2
Arnav <aarnavsalvi2007@gmail.com>
2026-06-20 14:19:44 +0530

    forloop

---
+++ forloop.java
@@ -0,0 +1,130 @@
+public class forloop {
+    public static void main(String[] args) {
+        //  while loop
+        // defn : The code repeatedly executes a block of code as long as condition is satisfied
+        // int count = -10;
+        // while (count <= 10 && count > 0)
+        // {
+        //     System.out.println(count);
+        //     count--;
+        // }
+
+        // do while
+        // defn : In do while loop that executes a block of code atleast once and then repeatedly executes as long as condition is satisfied.
+        
+        // do 
+        // {
+        //     System.out.println("My loop Starts");
+        //     count--;
+        // }
+        // while (count <= 10 && count > 5);
+
+        // int ans = 0;
+        // while (count > 0)
+        // {
+        //     ans = ans + count;
+        //     count --;
+       
+        // }
+        // System.out.println(ans);
+
+        // int num = 157;
+        //    int ans = 0;
+        // while (num > 0)
+        // {
+        //     int num1 = num % 10 ;
+        //     num = num/10;
+        //     ans = ans + num1;
+        // }
+        // System.out.println(ans);
+
+        // int num = 147896321;
+        // int ans = 0;
+        // while (num > 0)
+        // {
+        //     int num1 = num % 10 ;
+        //     if (num1 % 2 == 0)
+        //     {
+               
+        //          ans = ans + num1;
+        //     }
+        //     num = num / 10;
+           
+            
+        // }
+        //  System.out.println(ans);
+
+        // How to reverse number
+        // int num = 12;
+        // int ans = 0;
+        // while (num > 0)
+        // {
+        //     int num1 = num % 10 ;
+        //     num = num / 10;
+        //     ans = (ans * 10) + num1;
+            
+            
+        // }
+        // System.out.println(ans);
+
+
+        // Palindrome
+        // int num = 121;
+        // int num2 = num;
+        // int ans = 0;
+        // while (num > 0)
+        // {
+        //     int num1 = num % 10;
+        //     num = num / 10;
+        //     ans = (ans * 10) + num1;
+
+        // }
+        // if (ans == num2)
+        // {
+        //     System.out.println("Palindrome no.");
+        // }
+
+        // Fibonacci series
+
+        // int a = 0;
+        // int b = 1;
+        // int c = 0;
+        // int count = 0;
+        // int num = 5;
+        // while(num > count && count >= 0)
+        // {
+           
+        //     c = a + b;
+        //     a = b;
+        //     b = c;
+        //     System.out.println(a);
+           
+        //     count++;
+            
+
+
+
+        // }
+        
+
+
+        
+
+
+
+
+
+
+        
+
+
+
+        
+            
+        
+ 
+
+
+        
+    }
+}
