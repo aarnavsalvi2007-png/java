a02bbd07a440729deb37201354f969b7ad364d77
Arnav <aarnavsalvi2007@gmail.com>
2026-06-20 11:25:07 +0530

    Practice

---
+++ practice.java
@@ -0,0 +1,305 @@
+// Conditional concept practice
+
+public class practice {
+    public static void main(String[] args) {
+        
+        // int num1 = 2;
+        // int num2 = 3;
+        
+        // if (num1 > num2)
+        // {
+        //     System.out.println("num1 is greater");
+        // }
+        // else 
+        // {
+        //     System.out.println("num2 is greater");
+
+        // }
+
+        // int num = 18;
+        // if (num <= 18)
+        // {
+        //     System.out.println("person is eligible for voting");
+        // }
+        // else
+        // {
+        //     System.out.println("person is not eligible");
+        // }
+
+
+        // int num = 55;
+        // if (num % 5 == 0 && num % 11 == 0)
+        // {
+        //     System.out.println("no. is divisible by both");
+        // }
+        // else
+        // {
+        //     System.out.println("no. is not divisible by both");
+        // }
+
+        // int num1 = 15;
+        // int num2 = 20;
+        // int num3  = 36;
+
+        // if (num1 > num2 && num1 > num3)
+        // {
+        //     System.out.println("no.1 is greater");
+        // }
+        // else if ( num2 > num1 && num2 > num3)
+        // {
+        //     System.out.println("no.2 is greater");
+        // }
+        // else
+        // {
+        //     System.out.println("no.3 is greater");
+        // }
+
+        // int num = 2000;
+        // if (num % 400 == 0 || (num % 4 == 0 && num % 100 != 0))
+        // {
+        //     System.out.println("it is leap year");
+        // }
+        // else
+        // {
+        //     System.out.println("it is not a leap year");
+        // }
+
+        // int num = 45;
+        // if (num >= 90)
+        // {
+        //     System.out.println("grade A");
+        // }
+        // else if (num <= 89 && num >= 75)
+        // {
+        //     System.out.println("grade B");
+        // }
+        // else if (num <= 74 && num >= 50)
+        // {
+        //     System.out.println("grade C");
+        // }
+        // else if (num < 50)
+        // {
+        //     System.out.println("fail");
+
+        // }
+
+
+        // char ch = 'n';
+        // if (( ch >= 'A' && ch <= 'Z') || ( ch >= 'a' && ch <= 'z')) 
+        // {
+        //     System.out.println("its an alphabet");
+        // }
+        // else if ( ch >= '0' && ch <= '9')
+        // {
+        //     System.out.println("its a digit");
+        // }
+        // else
+        // {
+        //     System.out.println("its special character");
+        // }
+
+        // int num = 300;
+        // double bills;
+
+        // if ( num <= 100)
+        // {
+        //     bills = num * 5; 
+        // }
+        // else if ( num <= 200)
+        // {
+        //     bills = (100 * 5) + ((num - 100) * 7);
+        // }
+        // else
+        // {
+        //     bills = (100 * 5) + (100 * 7) + (( num - 200) * 10);
+        // }
+        // System.out.println(" bill = " + bills);
+        
+        // int sp = 200000;
+        // int cp = 1000;
+        // if (sp > cp)
+        // {
+        //     int profit = sp - cp;
+        //     System.out.println("profit = " + profit);
+        // }
+        // else if ( cp > sp)
+        // {
+        //     int loss = cp - sp;
+        //     System.out.println(" loss = " + loss);
+        // }
+        // else
+        // {
+        //     System.out.println("no profit no loss");
+        // }
+
+        // int num1 = 50;
+        // int num2 = 60;
+        // int num3 = 80;
+
+        // if (num1 + num2 + num3 == 180)
+        // {
+        //     System.out.println("Valid triangle");
+        // }
+        // else
+        // {
+        //     System.out.println("invalid triangle");
+        // }
+
+        // int enterpin = 1235;
+        // int correctpin = 1234;
+
+        // double withdrawal = 110000;
+        // double savings = 10000;
+
+        // if (enterpin == correctpin)
+        // {
+        //     if ( withdrawal <= savings )
+        //     {
+        //         savings = savings - withdrawal;
+        //         System.out.println("withdrawal successful");
+        //         System.out.println("remaining balance = " + savings);
+        //     }
+        //     else
+        //     {
+        //         System.out.println("withdrawal unsuccessful");
+        //     }
+        
+        // }
+        // else
+        // {
+        //     System.out.println("incorrect pin");
+        // }   
+
+        // int num = 9;
+        // if ( num == 1)
+        // {
+        //     System.out.println("its monday");
+        // }
+        // else if ( num == 2)
+        // {
+        //     System.out.println("its tuesday");
+        // }
+        // else if (num == 3)
+        // {
+        //     System.out.println("its wednesday");
+        // }
+        // else if (num == 4)
+        // {
+        //     System.out.println("its thursday");
+        // }
+        // else if ( num == 5)
+        // {
+        //     System.out.println("its friday");
+        // }
+        // else if ( num == 6)
+        // {
+        //     System.out.println("its saturday");
+        // }
+        // else if ( num == 7)
+        // {
+        //     System.out.println("its sunday");
+        // }
+        // else
+        // {
+        //     System.out.println(" invalid day ");
+        // }
+
+        // int num = 40;
+        // if ( num > 0 && num % 2 == 0)
+        // {
+        //     System.out.println("positive even no.");
+        // }
+        // else if ( num > 0 && num % 2 != 0)
+        // {
+        //     System.out.println("positive odd no.");
+        // }
+        // else if ( num < 0 && num % 2 == 0)
+        // {
+        //     System.out.println("negative even no.");
+        // }
+        // else if ( num < 0 && num % 2 != 0)
+        // {
+        //     System.out.println("negative odd no.");
+        // }
+        // else if ( num == 0)
+        // {
+        //     System.out.println("zero");
+        // }
+        // else 
+        // {
+        //     System.out.println("invalid integer");
+        // }
+
+        // String correctusername = "Arnav";
+        // String correctpassword = "9875";
+
+        // String enteredusername = "Arnav";
+        // String enteredpassword = "9875";
+
+        // if ( enteredusername.equals(correctusername))
+        // {
+        //     if (enteredpassword.equals(correctpassword))
+        //     {
+        //         System.out.println("Login Successful");
+        //     }
+        //     else
+        //     {
+        //         System.out.println("incorrect password");
+        //     }
+        // }
+        // else
+        // {
+        //     System.out.println("User not found");
+        // }
+
+    // operators.java practice
+
+    // int Maths = 85;
+    // int Science = 90;
+    // int SST = 80;
+    // int total  = Maths + Science + SST;
+    // System.out.println("Total marks = " + total);
+
+    // int savings = 1000;
+    // int expenses = 400;
+    // int total = savings - expenses;
+    // System.out.println("Total amount = " + total);
+
+    
+
+
+
+
+
+    
+    
+            
+        
+
+
+        
+        
+        
+
+        
+        
+
+        
+
+        
+        
+
+
+
+
+
+
+
+
+
+    
+
+        
+
+    }
+}
